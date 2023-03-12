77abac5a94af37ec26b73a02210e741b3635e5b5
Sivkova Alena <lipanissa9@gmail.com>
2023-03-12 22:19:44 +0700

    package tincture reading files

--- src/genDiff.js
+++ src/genDiff.js
@@ -4,31 +4,26 @@ const genDiff = (data1, data2) => {
   const keys1 = _.keys(data1);
   const keys2 = _.keys(data2);
   const keys = _.sortBy(_.union(keys1, keys2));
-
-  const compare = (acc, key) => {
-    // console.log(key);
-    const matchesKey = `  ${key}`;
-    const plusKey = `+ ${key}`;
-    const minusKey = `- ${key}`;
-
-    if (!Object.hasOwn(data2, key)) {
-      acc.push(`  ${minusKey}: ${data1[key]}`);
-    } else if (!Object.hasOwn(data1, key)) {
-      acc.push(`  ${plusKey}: ${data2[key]}`);
+  
+  const iter = (objData, key) => {
+   const emptiness = `  ${key}`;
+   const plus = `+ ${key}`;
+   const minus = `- ${key}`;
+   
+    if (!_.has(data2, key)) {
+      objData.push(`  ${minus}: ${data1[key]}`);
+    } else if (!_.has(data1, key)) {
+      objData.push(`  ${plus}: ${data2[key]}`);
     } else if (data1[key] !== data2[key]) {
-      acc.push(`  ${minusKey}: ${data1[key]}`);
-      acc.push(`  ${plusKey}: ${data2[key]}`);
+       objData.push(`  ${minus}: ${data1[key]}`);
+       objData.push(`  ${plus}: ${data2[key]}`); 
     } else {
-      acc.push(`  ${matchesKey}: ${data1[key]}`);
+      objData.push(`  ${emptiness}: ${data1[key]}`);
     }
-
-    // console.log(acc);
-    return acc;
-  };
-
-  const result = (keys.reduce(compare, []).join('\n'));
+    return objData;
+  }
+  const result = (keys.reduce(iter, []).join('\n'));
   result.toString();
   return `{\n${result}\n}`;
 };
-
 export default genDiff;
