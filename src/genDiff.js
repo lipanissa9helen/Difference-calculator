59ce7e6bacc7e0a8d7ecad27d01c397372508089
Sivkova Alena <lipanissa9@gmail.com>
2023-03-12 22:59:24 +0700

    eslint

--- src/genDiff.js
+++ src/genDiff.js
@@ -4,24 +4,24 @@ const genDiff = (data1, data2) => {
   const keys1 = _.keys(data1);
   const keys2 = _.keys(data2);
   const keys = _.sortBy(_.union(keys1, keys2));
-  
+
   const iter = (objData, key) => {
-   const emptiness = `  ${key}`;
-   const plus = `+ ${key}`;
-   const minus = `- ${key}`;
-   
+    const emptiness = `  ${key}`;
+    const plus = `+ ${key}`;
+    const minus = `- ${key}`;
+
     if (!_.has(data2, key)) {
       objData.push(`  ${minus}: ${data1[key]}`);
     } else if (!_.has(data1, key)) {
       objData.push(`  ${plus}: ${data2[key]}`);
     } else if (data1[key] !== data2[key]) {
-       objData.push(`  ${minus}: ${data1[key]}`);
-       objData.push(`  ${plus}: ${data2[key]}`); 
+      objData.push(`  ${minus}: ${data1[key]}`);
+      objData.push(`  ${plus}: ${data2[key]}`);
     } else {
       objData.push(`  ${emptiness}: ${data1[key]}`);
     }
     return objData;
-  }
+  };
   const result = (keys.reduce(iter, []).join('\n'));
   result.toString();
   return `{\n${result}\n}`;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import genDiff from './genDiff.js';
 
 export default (filepath1, filepath2) => {
   const data1 = readFileSync(filepath1, 'utf-8');
-  const data2 = readFileSync(filepath2, 'utf-8'); 
+  const data2 = readFileSync(filepath2, 'utf-8');
   const parseData1 = JSON.parse(data1);
   const parseData2 = JSON.parse(data2);
   return genDiff(parseData1, parseData2);
