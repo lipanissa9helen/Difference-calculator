0755298dc7ecb305bd2dc808c7fe529d956474e5
Sivkova Alena <lipanissa9@gmail.com>
2023-03-14 10:36:54 +0700

    add Github Actions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { readFileSync } from 'fs';
+import path from 'path';
+import { cwd } from 'process';
 import genDiff from './genDiff.js';
 
 export default (filepath1, filepath2) => {
-  const data1 = readFileSync(filepath1, 'utf-8');
-  const data2 = readFileSync(filepath2, 'utf-8');
+  const data1 = readFileSync(path.resolve(cwd(), filepath1), 'utf-8');
+  const data2 = readFileSync(path.resolve(cwd(), filepath2), 'utf-8');
   const parseData1 = JSON.parse(data1);
   const parseData2 = JSON.parse(data2);
+
   return genDiff(parseData1, parseData2);
 };
