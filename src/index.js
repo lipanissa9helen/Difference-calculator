4aacb6350e3b477f022d027d02a09540c7b29f16
Sivkova Alena <lipanissa9@gmail.com>
2023-03-20 11:37:29 +0700

    fix lint

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ export default (filepath1, filepath2) => {
   const path2 = getPath(filepath2);
   const data1 = readFileSync(filepath1, 'utf-8');
   const data2 = readFileSync(filepath2, 'utf-8');
-  
   const parseData1 = parse(data1, getFormat(path1));
   const parseData2 = parse(data2, getFormat(path2));
   return genDiff(parseData1, parseData2);
