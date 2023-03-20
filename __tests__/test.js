c35cc9c7ebae3de83a1842ec94bf609c8c7a5373
Sivkova Alena <lipanissa9@gmail.com>
2023-03-20 23:26:49 +0700

    fix test

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,6 +1,7 @@
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { expect, test } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,5 +13,11 @@ const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 test('file json', () => {
   const expected = readFile('result_json.txt');
   const actual = genDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'));
+  expect(actual).toBe(expected);
+});
+
+test('file yml', () => {
+  const expected = readFile('result_json.txt');
+  const actual = genDiff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml'));
   expect(actual).toBe(expected);
 });
