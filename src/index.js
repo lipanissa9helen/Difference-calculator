832f44dba44d159f90d566d813badcbe99e90f55
Sivkova Alena <lipanissa9@gmail.com>
2023-04-02 23:24:13 +0700

    fix index

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import buildTree from './buildTree.js';
 import parse from './parsers.js';
 import formatTree from './formatters/index.js';
 
-const getPath = (filepath) => readFileSync(path.resolve(cwd(), filepath));
+const readFile = (filepath) => readFileSync(path.resolve(cwd(), filepath));
 const getFormat = (fileName) => path.extname(fileName).slice(1);
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
-  const parseData1 = parse(getPath(filepath1), getFormat(filepath1));
-  const parseData2 = parse(getPath(filepath2), getFormat(filepath2));
+  const parseData1 = parse(readFile(filepath1), getFormat(filepath1));
+  const parseData2 = parse(readFile(filepath2), getFormat(filepath2));
   return formatTree(buildTree(parseData1, parseData2), formatName);
 };
